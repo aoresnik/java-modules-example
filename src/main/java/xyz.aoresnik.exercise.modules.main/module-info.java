be2132db82d1f34b6749dd7948e5e0aaa8816f86
Andrej Orešnik <aoresnik7@gmail.com>
2021-03-01 00:59:18 +0100

    Main modules don't need to export packages

--- src/main/java/xyz.aoresnik.exercise.modules.main/module-info.java
+++ src/main/java/xyz.aoresnik.exercise.modules.main/module-info.java
@@ -1,5 +1,4 @@
 module xyz.aoresnik.exercise.modules.main {
-    exports xyz.aoresnik.exercise.modules.main;
     requires xyz.aoresnik.exercise.modules.module_b;
     requires xyz.aoresnik.exercise.modules.serviceintf;
     requires xyz.aoresnik.exercise.modules.openmodule;
--- src/main/java/xyz.aoresnik.exercise.modules.main_noauto/module-info.java
+++ src/main/java/xyz.aoresnik.exercise.modules.main_noauto/module-info.java
@@ -1,5 +1,4 @@
 module xyz.aoresnik.exercise.modules.main_noauto {
-    exports xyz.aoresnik.exercise.modules.main;
     requires xyz.aoresnik.exercise.modules.module_b;
     requires xyz.aoresnik.exercise.modules.serviceintf;
     requires xyz.aoresnik.exercise.modules.openmodule;
